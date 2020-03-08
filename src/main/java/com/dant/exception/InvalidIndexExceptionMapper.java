8fea03cc1883221ef744831c77c755929de89579
Massil Taguemout <massitaguemout@gmail.com>
2020-03-08 13:59:19 +0100

    add InvalidIndexExceptionMapper

---
+++ src/main/java/com/dant/exception/InvalidIndexExceptionMapper.java
@@ -0,0 +1,12 @@
+package com.dant.exception;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+public class InvalidIndexExceptionMapper implements ExceptionMapper<InvalidIndexException> {
+
+	@Override
+	public Response toResponse(InvalidIndexException e) {
+		return Response.status(400).entity(e.getMessage()).type("plain/text").build();
+	}
+}
